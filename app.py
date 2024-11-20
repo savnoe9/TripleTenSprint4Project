f7faf29d02cdbf3bd7ab26ae575165718f384cc6
SavNoe0 <savannahnoe95@gmail.com>
2024-11-20 07:43:06 -0600

    Updating virtual environment and adding app.py file

---
+++ app.py
@@ -0,0 +1,17 @@
+#Import appropriate libraries
+import pandas as pd
+import streamlit as st
+from scipy import stats as sta
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly_express as px
+
+#Import dataset
+df = pd.read_csv('vehicles_us.csv')
+#df.info()
+
+st.write("""
+         ##Vehicles by Make
+          """)
+
+#st.scatter_chart()
