635cf93c41e581df80abe4cd42c6b72d88744c73
SavNoe0 <savannahnoe95@gmail.com>
2024-11-22 17:03:30 -0600

    Making myapp updates

--- app.py
+++ app.py
@@ -10,8 +10,56 @@ import plotly_express as px
 df = pd.read_csv('vehicles_us.csv')
 #df.info()
 
+#Drop rows where model year or odometer reading is missing
+df=df.dropna(subset=['model_year'])
+df=df.dropna(subset=['odometer'])
+
+#Convert columns to appropriate data types
+df['date_posted'] = pd.to_datetime(df['date_posted'])
+df['is_4wd'] = df['is_4wd'].fillna(value=0)
+df['is_4wd'] = df['is_4wd'].astype('int')
+df['cylinders'] = df['cylinders'].fillna(value=0)
+df['cylinders'] = df['cylinders'].astype('int')
+df['model_year'] = df['model_year'].astype('int')
+df['odometer'] = df['odometer'].astype('int')
+
+#Add new column - manufacturer
+df['manufacturer'] = df['model'].apply(lambda x:x.split()[0])
+
+#Add new column - luxury vs standard
+luxury_brands = ['bmw','mercedes-benz','acura','cadillac']
+luxury_class = []
+for value in df['manufacturer']:
+    if value in luxury_brands:
+        luxury_class.append('luxury')
+    else:
+        luxury_class.append('normal')
+df['luxury_class'] = luxury_class
+
+
+
+st.write("""
+         ##View Vehicle Advertisement Dataset
+          """)
+TenK = st.checkbox("Show vehicles under $10,000")
+
+if TenK:
+    st.write(print(df.loc[df['price'] < 10000]))
+else:
+    print(df)
+
 st.write("""
          ##Vehicles by Make
           """)
 
 #st.scatter_chart()
+
+price_vs_odometer = px.scatter(
+    df,
+    x='odometer',
+    y='price',
+    title='Price vs. Odometer',
+    opacity=0.5 
+)
+
+price_vs_odometer.show()
