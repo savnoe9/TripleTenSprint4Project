52fa226a03ba186579d08022143a6d15756f6523
SavNoe0 <savannahnoe95@gmail.com>
2024-11-24 19:42:45 -0600

    testing

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 #Import appropriate libraries
 import pandas as pd
 import streamlit as st
-from scipy import stats as sta
-import numpy as np
+#from scipy import stats as sta
+#import numpy as np
 #import matplotlib.pyplot as plt
-#import plotly_express as px
+import plotly_express as px
 
 #Import dataset
 df = pd.read_csv('vehicles_us.csv')
@@ -62,3 +62,24 @@ st.write("""
 
 
 #st.write(price_vs_odometer)
+
+import streamlit as st
+import numpy as np
+import plotly.figure_factory as ff
+
+# Add histogram data
+x1 = np.random.randn(200) - 2
+x2 = np.random.randn(200)
+x3 = np.random.randn(200) + 2
+
+# Group data together
+hist_data = [x1, x2, x3]
+
+group_labels = ['Group 1', 'Group 2', 'Group 3']
+
+# Create distplot with custom bin_size
+fig = ff.create_distplot(
+        hist_data, group_labels, bin_size=[.1, .25, .5])
+
+# Plot!
+st.plotly_chart(fig, use_container_width=True)
