66ae7de7fa008a30f278c71c83a70dea0e6e97bb
SavNoe0 <savannahnoe95@gmail.com>
2024-11-26 20:37:45 -0600

    Adding more plots

--- app.py
+++ app.py
@@ -75,3 +75,70 @@ scatdf = scatdf1.loc[scatdf1['odometer'] < 600000]
 scat = px.scatter(scatdf, x='price', y='odometer', opacity=0.5)
 
 event = st.plotly_chart(scat, on_select="rerun")
+
+
+model_list = df['model'].value_counts().sort_index()
+model_list.columns=['model','sales']
+#print(model_list)
+
+
+
+modelbar = px.bar(
+                df,
+                title= 'Distribution of Models Sold',
+                x = 'model', 
+                labels={'model': 'Vehicle Model'},
+                color_discrete_sequence=px.colors.qualitative.Set1,
+                width=1600,
+                height=1000)
+
+st.plotly_chart(modelbar)
+
+
+st.subheader('Vehicle Types by Class')
+
+lux_hist = px.histogram(df,x='luxury_class',color='type')
+st.write(lux_hist)
+
+
+st.subheader('Condition vs. Model Year')
+
+cond_year_hist = px.histogram(df,x='model_year',color='condition')
+st.write(cond_year_hist)
+
+
+
+
+# get user's inputs from a dropdown menu
+manufac_list = sorted(df['manufacturer'].unique())
+manufacturer_1 = st.selectbox(
+                              label='Select manufacturer 1', # title of the select box
+                              options=manufac_list, # options listed in the select box
+                              index=manufac_list.index('chevrolet') # default pre-selected option
+                              )
+# repeat for the second dropdown menu
+manufacturer_2 = st.selectbox(
+                              label='Select manufacturer 2',
+                              options=manufac_list, 
+                              index=manufac_list.index('hyundai')
+                              )
+# filter the dataframe 
+mask_filter = (df['manufacturer'] == manufacturer_1) | (df['manufacturer'] == manufacturer_2)
+df_filtered = df[mask_filter]
+
+# add a checkbox if a user wants to normalize the histogram
+normalize = st.checkbox('Normalize histogram', value=True)
+if normalize:
+    histnorm = 'percent'
+else:
+    histnorm = None
+
+# create a plotly histogram figure
+filtered_hist = px.histogram(df_filtered,
+                      x='price',
+                      nbins=30,
+                      color='manufacturer',
+                      histnorm=histnorm,
+                      barmode='overlay')
+
+st.write(filtered_hist)
