842c38bd9e0cd88fd6592dc95010cf2f3dcd3353
SavNoe0 <savannahnoe95@gmail.com>
2024-11-27 22:28:05 -0600

    Fixes

--- app.py
+++ app.py
@@ -23,6 +23,13 @@ df['cylinders'] = df['cylinders'].fillna(value=0)
 df['cylinders'] = df['cylinders'].astype('int')
 df['model_year'] = df['model_year'].astype('int')
 df['odometer'] = df['odometer'].astype('int')
+df['paint_color'] = df['paint_color'].fillna(value='Unknown')
+
+# Calculate the median cylinders for each model_year, fill in missing values
+median_cylinders = df.groupby(['model_year', 'model'])['cylinders'].transform('median')
+df['cylinders'] = df['cylinders'].fillna(median_cylinders)
+df['cylinders'] = df['cylinders'].fillna(value=0)
+df['cylinders'] = df['cylinders'].astype('int')
 
 #Add new column - manufacturer
 df['manufacturer'] = df['model'].apply(lambda x:x.split()[0])
@@ -53,7 +60,6 @@ else:
 st.subheader("Vehicle Types Compared to Days Listed")
 
 #Create Histogram
-
 fig = px.histogram(
     df,
     x='days_listed',
@@ -64,6 +70,7 @@ fig = px.histogram(
 
 st.plotly_chart(fig)
 
+#We can see here that most vehicles are listed within a range of 8-50 days for all types of vehicles.
 
 st.subheader("Price vs. Odometer Reading")
 
@@ -71,22 +78,38 @@ st.subheader("Price vs. Odometer Reading")
 scatdf1 = df.loc[df['price'] < 100000]
 scatdf = scatdf1.loc[scatdf1['odometer'] < 600000]
 
-scat = px.scatter(scatdf, x='price', y='odometer', opacity=0.5)
+scat = px.scatter(scatdf, x='price', y='odometer', opacity=0.5, color ='manufacturer')
 
 event = st.plotly_chart(scat, on_select="rerun")
 
+#We can see that there are some outliers in both price and odometer reading based on this scatterplot. However, it is clear that most vehicles sell for under $50,000 with less than 200,000 miles on them.
 
+
+st.subheader('Distribution of Sold Vehicles by Manufacturer')
+manubar = px.bar(
+                df,
+                x = 'manufacturer', 
+                labels={'manufacturer': 'Vehicle Manufacturer'},
+                width=1600,
+                height=1000)
+
+st.plotly_chart(manubar)
 
 st.subheader('Distribution of Models Sold')
 modelbar = px.bar(
                 df,
                 x = 'model', 
                 labels={'model': 'Vehicle Model'},
-                color_discrete_sequence=px.colors.qualitative.Set1,
                 width=1600,
                 height=1000)
 
 st.plotly_chart(modelbar)
+
+
+st.subheader('Vehicle Manufactureres and Models')
+
+manu_hist = px.histogram(df,x='manufacturer',color='model', color_discrete_sequence=px.colors.qualitative.Set1 )
+st.write(manu_hist)
 
 
 st.subheader('Vehicle Types by Class')
